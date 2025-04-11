a3e583fe4692961acb732d4e90650f5341b0f481
paulockotchetkoff <paulockotchetkoff@gmail.com>
2025-04-11 03:35:06 -0300

    Add response iteration

--- dags/ingest_data.py
+++ dags/ingest_data.py
@@ -11,12 +11,15 @@ PIPELINE_BUCKET = os.environ['PIPELINE_BUCKET']
 BASE_URL = 'https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{0}-{1}.csv'
 
 
-def upload_to_gcs(bucket_name, object_name, data):
+def upload_to_gcs(bucket_name, object_name, response):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(object_name)
     
-    blob.upload_from_string(data)
+    # blob.upload_from_string(data)
+    with blob.open('wb') as gcs_file:
+        for chunk in response.iter_content(chunk_size=8*1024*1024):
+            gcs_file.write(chunk)
         
     print(f'Uploaded {object_name} to {bucket_name}')
 
@@ -30,7 +33,7 @@ def download_fuel_data():
             response = requests.get(BASE_URL.format(year, semester), stream=True)
             response.raise_for_status()
             
-            upload_to_gcs(PIPELINE_BUCKET, output_file_name, response.content)
+            upload_to_gcs(PIPELINE_BUCKET, output_file_name, response)
 
     return 'Data successfully stored in GCS'
 

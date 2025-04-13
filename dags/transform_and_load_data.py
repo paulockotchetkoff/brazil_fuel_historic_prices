01aac5ab0d6befa5e388777c15ee41a2fd6cb854
paulockotchetkoff <paulockotchetkoff@gmail.com>
2025-04-12 23:07:36 -0300

    Fix variables

--- dags/transform_and_load_data.py
+++ dags/transform_and_load_data.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+import os
 
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
+
+PIPELINE_BUCKET = os.environ['PIPELINE_BUCKET']
+GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
+BQ_DATASET_ID = os.environ['BQ_DATASET_ID']
 
 default_args = {
     'owner': 'airflow',
@@ -19,7 +24,7 @@ with DAG(
 
     spark_job = SparkSubmitOperator(
         task_id='process_fuel_prices',
-        application="gs://{{ var.value.PIPELINE_BUCKET }}/spark_jobs/bq_test.py",
+        application=f'gs://{PIPELINE_BUCKET}/spark_jobs/bq_test.py',
         conn_id='spark_default',
         conf={
             'spark.jars.packages': 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.32.2',
@@ -27,7 +32,7 @@ with DAG(
             'spark.hadoop.fs.gs.auth.service.account.enable': 'true'
         },
         env_vars={
-            'INPUT_PATH': 'gs://{{ var.value.PIPELINE_BUCKET }}/fuel_prices_2004_01.csv',
-            'BQ_TABLE': '{{ var.value.GCP_PROJECT_ID }}.{{ var.value.BQ_DATASET_ID }}.test'
+            'INPUT_PATH': f'gs://{PIPELINE_BUCKET}/fuel_prices_2004_01.csv',
+            'BQ_TABLE': f'{GCP_PROJECT_ID}.{BQ_DATASET_ID}.test'
         }
     )

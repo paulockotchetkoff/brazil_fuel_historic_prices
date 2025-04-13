74ba4eeaf36cde59a2211897c1607be1e73b56d9
paulockotchetkoff <paulockotchetkoff@gmail.com>
2025-04-13 02:38:54 -0300

    Testing PySpark

--- dags/transform_and_load_data.py
+++ dags/transform_and_load_data.py
@@ -2,12 +2,12 @@ from datetime import datetime
 import os
 
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-
+from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
 
 PIPELINE_BUCKET = os.environ['PIPELINE_BUCKET']
 GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
 BQ_DATASET_ID = os.environ['BQ_DATASET_ID']
+REGION = os.environ['GCP_REGION']
 
 default_args = {
     'owner': 'airflow',
@@ -16,23 +16,31 @@ default_args = {
 }
 
 with DAG(
-    'testing_spark',
+    'fuel_prices_processing',
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    spark_job = SparkSubmitOperator(
+    spark_job = DataprocSubmitJobOperator(
         task_id='process_fuel_prices',
-        application=f'gs://{PIPELINE_BUCKET}/spark_jobs/bq_test.py',
-        conn_id='spark_default',
-        conf={
-            'spark.jars.packages': 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.32.2',
-            'spark.hadoop.fs.gs.impl': 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem',
-            'spark.hadoop.fs.gs.auth.service.account.enable': 'true'
-        },
-        env_vars={
-            'INPUT_PATH': f'gs://{PIPELINE_BUCKET}/fuel_prices_2004_01.csv',
-            'BQ_TABLE': f'{GCP_PROJECT_ID}.{BQ_DATASET_ID}.test'
+        project_id=GCP_PROJECT_ID,
+        region=REGION,
+        job={
+            'reference': {'job_id': 'fuel-prices-job-{{ ds_nodash }}'},
+            'placement': {'cluster_name': ''},  # Serverless
+            'spark_job': {
+                'main_python_file_uri': f'gs://{PIPELINE_BUCKET}/spark_jobs/bq_test.py',
+                'args': [
+                    f'--input_path=gs://{PIPELINE_BUCKET}/fuel_prices_2004_01.csv',
+                    f'--bq_table={GCP_PROJECT_ID}.{BQ_DATASET_ID}.test_tbl',
+                    f'--temp_bucket={GCP_PROJECT_ID}-spark-temp'  # From Terraform
+                ],
+                'jar_file_uris': ['gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'],
+                'properties': {
+                    'spark.submit.deployMode': 'cluster',
+                    'spark.sql.legacy.timeParserPolicy': 'LEGACY'  # For datetime parsing
+                }
+            }
         }
     )
--- spark_jobs/bq_test.py
+++ spark_jobs/bq_test.py
@@ -1,18 +1,41 @@
-import os
+import argparse
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_path', required=True)
+    parser.add_argument('--bq_table', required=True)
+    parser.add_argument('--temp_bucket', required=True)
+    args = parser.parse_args()
+
     spark = SparkSession.builder \
-        .appName('testing_pyspark') \
+        .appName('testing-pyspark') \
+        .config('spark.sql.execution.arrow.pyspark.enabled', 'true') \
         .getOrCreate()
 
-    df = spark.read.options(delimiter=';', header=True).csv(os.environ['INPUT_PATH'])
-    
-    df.write \
-        .format('bigquery') \
-        .option('table', os.environ['BQ_TABLE']) \
+    # Read CSV with error handling
+    df = spark.read \
+        .options(
+            delimiter=';',
+            header=True,
+            inferSchema=True,
+            encoding='UTF-8',
+            dateFormat='dd/MM/yyyy'
+        ) \
+        .csv(args.input_path)
+
+
+    df.write.format('bigquery') \
+        .option('table', args.bq_table) \
+        .option('temporaryGcsBucket', args.temp_bucket) \
         .mode('overwrite') \
         .save()
 
 if __name__ == '__main__':
     main()
+
+# .option('writeDisposition', 'WRITE_TRUNCATE') \
+# .option('partitionField', 'date') \
+# .option('clusteredFields', 'state,fuel_type') \

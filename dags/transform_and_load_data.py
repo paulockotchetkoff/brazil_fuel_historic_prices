8d7a0a690c45d7203c3398f2c5cba788f359df00
paulockotchetkoff <paulockotchetkoff@gmail.com>
2025-04-13 20:28:30 -0300

    Change file structure

--- dags/transform_and_load_data.py
+++ dags/transform_and_load_data.py
@@ -1,72 +1,64 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 import os
 
-from airflow import DAG
 from airflow import models
-from airflow.providers.google.cloud.operators.dataproc import (
-    DataprocCreateClusterOperator,
-    DataprocSubmitJobOperator,
-    DataprocDeleteClusterOperator
-)
+from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
+from airflow.utils.dates import days_ago
 
-PIPELINE_BUCKET = os.environ['PIPELINE_BUCKET']
-GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
 BQ_DATASET_ID = os.environ['BQ_DATASET_ID']
+BQ_JAR = 'gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'
+GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
+PIPELINE_BUCKET = os.environ['PIPELINE_BUCKET']
+PYSPARK_SCRIPT = f'gs://{os.environ["PIPELINE_BUCKET"]}/spark_jobs/bq_test.py'
 REGION = os.environ['GCP_REGION']
+SERVICE_ACCOUNT = 'composer-worker-sa@brazil-fuel-prices.iam.gserviceaccount.com'
 
-CLUSTER_CONFIG = {
-    "master_config": {"num_instances": 1, "machine_type_uri": "n1-standard-4"},
-    "worker_config": {"num_instances": 2, "machine_type_uri": "n1-standard-4"},
-    "gce_cluster_config": {
-        "service_account": "composer-worker-sa@brazil-fuel-prices.iam.gserviceaccount.com",
-        # "service_account_scopes": ["https://www.googleapis.com/auth/cloud-platform"]
-    }
-}
-
-PYSPARK_JOB = {
-    "reference": {"project_id": GCP_PROJECT_ID},
-    "placement": {"cluster_name": "test-cluster"},
-    "pyspark_job": {
-        "main_python_file_uri": f"gs://{PIPELINE_BUCKET}/spark_jobs/bq_test.py",
-        "jar_file_uris": ["gs://spark-lib/bigquery/spark-bigquery-latest.jar"]
-    },
-    'environment_config': {
-                'execution_config': {
-                    'service_account': 'composer-worker-sa@brazil-fuel-prices.iam.gserviceaccount.com',
-                }
-    }
-}
-
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 1, 1),
-    'retries': 2
-}
-
-with DAG(
-    "another_test",
+with models.DAG(
+    'bq_csv_processing',
     schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'retries': 1,
+    },
+    tags=['pyspark', 'bigquery'],
 ) as dag:
 
-    create_cluster = DataprocCreateClusterOperator(
-        task_id="create_cluster",
-        cluster_name="pyspark-cluster",
+    spark_task = DataprocCreateBatchOperator(
+        task_id='csv_to_bigquery',
         project_id=GCP_PROJECT_ID,
         region=REGION,
-        cluster_config=CLUSTER_CONFIG
+        batch={
+            'pyspark_batch': {
+                'main_python_file_uri': PYSPARK_SCRIPT,
+                'jar_file_uris': [BQ_JAR],
+                'args': [
+                    f'--input_path=gs://{PIPELINE_BUCKET}/fuel_prices_2004_01.csv',
+                    f'--bq_table={GCP_PROJECT_ID}.{BQ_DATASET_ID}.test',
+                    f'--temp_bucket={PIPELINE_BUCKET}-spark-temp'
+                ]
+            },
+            'runtime_config': {
+                'version': '2.1',
+                'properties': {
+                    'spark.sql.execution.arrow.pyspark.enabled': 'true',
+                    'spark.executor.instances': '4',
+                    'spark.executor.memory': '8g',
+                    'spark.driver.memory': '4g'
+                }
+            },
+            'environment_config': {
+                'execution_config': {
+                    'service_account': SERVICE_ACCOUNT,
+                    'service_account_scopes': ['https://www.googleapis.com/auth/cloud-platform']
+                }
+            }
+        },
+        batch_id=f'bq-load-{datetime.now().strftime('%Y%m%d-%H%M%S')}',
+        # timeout=3600
     )
 
-    submit_job = DataprocSubmitJobOperator(
-        task_id="run_pyspark",
-        job=PYSPARK_JOB,
-        region=REGION
-    )
-
-    delete_cluster = DataprocDeleteClusterOperator(
-        task_id="delete_cluster",
-        cluster_name="pyspark-cluster",
-        project_id=GCP_PROJECT_ID,
-        region=REGION
-    )
-
-    create_cluster >> submit_job >> delete_cluster
+    spark_task

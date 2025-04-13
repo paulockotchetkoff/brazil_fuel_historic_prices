7b11391bfa3a466bb8a485258fc046e3e8203258
paulockotchetkoff <paulockotchetkoff@gmail.com>
2025-04-13 02:56:13 -0300

    Fix arguments

--- dags/transform_and_load_data.py
+++ dags/transform_and_load_data.py
@@ -27,19 +27,21 @@ with DAG(
         project_id=GCP_PROJECT_ID,
         region=REGION,
         job={
-            'reference': {'job_id': 'fuel-prices-job-{{ ds_nodash }}'},
-            'placement': {'cluster_name': ''},
-            'pyspark_job': {
+            'reference': {'job_id': f'fuel-prices-job-{datetime.now().strftime("%Y%m%d%H%M%S")}'},
+            'spark_batch': {  # Changed from pyspark_job to spark_batch
                 'main_python_file_uri': f'gs://{PIPELINE_BUCKET}/spark_jobs/bq_test.py',
                 'args': [
                     f'--input_path=gs://{PIPELINE_BUCKET}/fuel_prices_2004_01.csv',
                     f'--bq_table={GCP_PROJECT_ID}.{BQ_DATASET_ID}.test',
                     f'--temp_bucket={GCP_PROJECT_ID}-spark-temp'
                 ],
-                'python_file_uris': ['gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'],
-                'properties': {
-                    'spark.submit.deployMode': 'cluster',
-                    'spark.sql.legacy.timeParserPolicy': 'LEGACY'
+                'jar_files': ['gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.32.2.jar'],
+                'runtime_config': {
+                    'version': '2.1',  # Dataproc Serverless version
+                    'properties': {
+                        'spark.submit.deployMode': 'cluster',
+                        'spark.sql.legacy.timeParserPolicy': 'LEGACY'
+                    }
                 }
             }
         }

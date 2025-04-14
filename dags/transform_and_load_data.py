22af6e20bf2d952fb1410d542034065c2ff6baea
paulockotchetkoff <paulockotchetkoff@gmail.com>
2025-04-13 21:07:17 -0300

    Testing

--- dags/transform_and_load_data.py
+++ dags/transform_and_load_data.py
@@ -1,64 +1,55 @@
-from datetime import datetime
+from datetime import datetime 
 import os
 
-from airflow import models
+from airflow import DAG
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
-from airflow.utils.dates import days_ago
 
-BQ_DATASET_ID = os.environ['BQ_DATASET_ID']
-BQ_JAR = 'gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'
-GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
 PIPELINE_BUCKET = os.environ['PIPELINE_BUCKET']
-PYSPARK_SCRIPT = f'gs://{os.environ["PIPELINE_BUCKET"]}/spark_jobs/bq_test.py'
+GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
+BQ_DATASET_ID = os.environ['BQ_DATASET_ID']
 REGION = os.environ['GCP_REGION']
-SERVICE_ACCOUNT = 'composer-worker-sa@brazil-fuel-prices.iam.gserviceaccount.com'
 
-with models.DAG(
-    'bq_csv_processing',
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 2
+}
+
+with DAG(
+    'fuel_prices_processing',
+    default_args=default_args,
     schedule_interval=None,
-    start_date=days_ago(1),
     catchup=False,
-    default_args={
-        'owner': 'airflow',
-        'depends_on_past': False,
-        'email_on_failure': False,
-        'retries': 1,
-    },
-    tags=['pyspark', 'bigquery'],
 ) as dag:
 
-    spark_task = DataprocCreateBatchOperator(
-        task_id='csv_to_bigquery',
+    spark_batch = DataprocCreateBatchOperator(
+        task_id='process_fuel_prices',
         project_id=GCP_PROJECT_ID,
         region=REGION,
+        batch_id=f'fuel-prices-batch-{datetime.now().strftime("%Y%m%d%H%M%S")}',
         batch={
             'pyspark_batch': {
-                'main_python_file_uri': PYSPARK_SCRIPT,
-                'jar_file_uris': [BQ_JAR],
+                'main_python_file_uri': f'gs://{PIPELINE_BUCKET}/spark_jobs/bq_test.py',
                 'args': [
                     f'--input_path=gs://{PIPELINE_BUCKET}/fuel_prices_2004_01.csv',
                     f'--bq_table={GCP_PROJECT_ID}.{BQ_DATASET_ID}.test',
-                    f'--temp_bucket={PIPELINE_BUCKET}-spark-temp'
-                ]
+                    f'--temp_bucket={GCP_PROJECT_ID}-spark-temp'
+                ],
+                'jar_file_uris': [
+                    'gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.32.2.jar'
+                ],
             },
             'runtime_config': {
-                'version': '2.1',
+                'version': '2.2',
                 'properties': {
-                    'spark.sql.execution.arrow.pyspark.enabled': 'true',
-                    # 'spark.dataproc.executor.machineType': 'n1-standard-2'
-                    # 'spark.dynamicAllocation.enabled': 'true',
-                    # 'spark.dynamicAllocation.maxExecutors': '4'
+                    'spark.sql.catalogImplementation': 'hive',
+                    'spark.sql.legacy.parquet.datetimeRebaseModeInWrite': 'CORRECTED'
                 }
             },
             'environment_config': {
                 'execution_config': {
-                    'service_account': SERVICE_ACCOUNT,
-                    # 'service_account_scopes': ['https://www.googleapis.com/auth/cloud-platform']
+                    'service_account': 'composer-worker-sa@brazil-fuel-prices.iam.gserviceaccount.com',
                 }
             }
-        },
-        batch_id=f'bq-load-{datetime.now().strftime("%Y%m%d-%H%M%S")}',
-        # timeout=3600
+        }
     )
-
-    spark_task
